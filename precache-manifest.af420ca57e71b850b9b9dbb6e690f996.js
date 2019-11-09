39bd880105e08841337f6104a0a9f99371f831a7
Jéssica Happatsch <jessica.p.happatsch@accenture.com>
2019-11-08 23:52:21 -0200

    Updates

---
+++ precache-manifest.af420ca57e71b850b9b9dbb6e690f996.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3dcf8cf68b9085f2fc9ba0b93ff2d6",
+    "url": "/another-react-app/index.html"
+  },
+  {
+    "revision": "adf6d323e9ff6a754176",
+    "url": "/another-react-app/static/css/main.aab3c4a9.chunk.css"
+  },
+  {
+    "revision": "dc1def3b5729b83bffd3",
+    "url": "/another-react-app/static/js/2.0426ef5d.chunk.js"
+  },
+  {
+    "revision": "adf6d323e9ff6a754176",
+    "url": "/another-react-app/static/js/main.c0919802.chunk.js"
+  },
+  {
+    "revision": "99897b4f753a8bb0e40f",
+    "url": "/another-react-app/static/js/runtime-main.00ec49f1.js"
+  }
+]);
